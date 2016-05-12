f7faff403dcbe8d8a80fa1329188830d941b960d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-05-12 14:43:16 +0900

    D-5
    s-1.0#1
    ---
    1. execute external => output --> to JLabel
    
        ==> done

--- src/sp/main/MainWindow.java
+++ src/sp/main/MainWindow.java
@@ -27,7 +27,18 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JLabel;
+
 import java.awt.SystemColor;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JTextField;
 
 public class MainWindow {
 
@@ -40,12 +51,28 @@ public class MainWindow {
 	private JPanel panel_1;
 	private JMenuItem mntmQuit;
 	private static JLabel lblMessage;
+	private JPanel panel_2;
+	private JTextField tf_Param1;
+	private JTextField tf_Param2;
+	private JLabel lbl_Param_1;
+	private JLabel lbl_Param_2;
+	private JLabel lblNewLabel;
+	private JPanel panel_3;
+	private static JLabel lbl_Output;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		//test
+//		File f = new File();
+//		
+//		f.de
+		
+		
 		EventQueue.invokeLater(new Runnable() {
+			
 			public void run() {
 				try {
 					MainWindow window = new MainWindow();
@@ -137,6 +164,40 @@ public class MainWindow {
 		lblMessage.setBackground(SystemColor.inactiveCaption);
 		lblMessage.setFont(new Font("MS UI Gothic", Font.PLAIN, 30));
 		panel_1.add(lblMessage);
+		
+		panel_2 = new JPanel();
+		frame.getContentPane().add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		lbl_Param_1 = new JLabel("param 1");
+		lbl_Param_1.setFont(new Font("MS UI Gothic", Font.PLAIN, 30));
+		panel_2.add(lbl_Param_1);
+		
+		tf_Param1 = new JTextField();
+		tf_Param1.setBackground(new Color(173, 255, 47));
+		panel_2.add(tf_Param1);
+		tf_Param1.setColumns(10);
+		
+		lbl_Param_2 = new JLabel("param 2");
+		lbl_Param_2.setFont(new Font("MS UI Gothic", Font.PLAIN, 30));
+		panel_2.add(lbl_Param_2);
+		
+		tf_Param2 = new JTextField();
+		tf_Param2.setBackground(SystemColor.inactiveCaption);
+		panel_2.add(tf_Param2);
+		tf_Param2.setColumns(10);
+		
+		lblNewLabel = new JLabel("New label");
+		panel_2.add(lblNewLabel);
+		
+		panel_3 = new JPanel();
+		frame.getContentPane().add(panel_3, BorderLayout.SOUTH);
+		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		lbl_Output = new JLabel("New label");
+		lbl_Output.setBackground(SystemColor.activeCaption);
+		lbl_Output.setFont(new Font("MS UI Gothic", Font.PLAIN, 40));
+		panel_3.add(lbl_Output);
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setFont(new Font("メイリオ", Font.PLAIN, 25));
@@ -232,6 +293,59 @@ public class MainWindow {
 		btnExec = new JButton("Exec");
 		
 		btn2 = new JButton("Button2");
+		
+		
+		
+		btn2.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+			
+				Process process;
+				try {
+					process = new ProcessBuilder(
+							
+							"C:\\WORKS\\Programs\\Java\\jdk1.7.0_79\\bin\\java.exe", 
+							"Test")
+	//					"C:\\WORKS\\WS\\Eclipse_Luna\\J_SoundProg\\java", "Test")
+						.start();
+					
+//				"C:\\PathToExe\\MyExe.exe","param1","param2").start();
+					
+					InputStream is = process.getInputStream();
+					
+					InputStreamReader isr = new InputStreamReader(is);
+					
+					BufferedReader br = new BufferedReader(isr);
+					
+					String line;
+					
+//				System.out.printf("Output of running %s is:", Arrays.toString(args));
+					
+					String whole_text = "";
+					
+					while ((line = br.readLine()) != null) {
+						
+						System.out.println(line);
+						
+						whole_text += line;
+						
+//						MainWindow..lbl_Output.setText(whole_text);
+//						MainWindow.lblMessage.setText("This lesson covers writing layout");
+						
+					}
+					
+					MainWindow.lbl_Output.setText(whole_text);
+//					MainWindow.lblMessage.setText(whole_text);
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}//public void actionPerformed(ActionEvent arg0)
+			
+		});
+		
 		
 		btnQuit = new JButton("Quit");
 
